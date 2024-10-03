5c8e7da954f7740ef850c1b9b97e7e78c361f6c0
Rafael Tomelin <rafael.tomelin@gmail.com>
2024-10-03 16:57:27 -0300

    create codecov.yaml

--- internal/entity/commit_test.go
+++ internal/entity/commit_test.go
@@ -30,3 +30,39 @@ func TestNewCommit(t *testing.T) {
 		})
 	})
 }
+
+func TestCommit_EnableEmoji(t *testing.T) {
+	t.Run("Commit.EnableEmoji", func(t *testing.T) {
+		t.Parallel()
+		t.Run("should return an array of booleans", func(t *testing.T) {
+			t.Parallel()
+		})
+	})
+}
+
+func TestCommit_Validate(t *testing.T) {
+	t.Run("Commit.Validate", func(t *testing.T) {
+		t.Parallel()
+		t.Run("should return an error when the subject is empty", func(t *testing.T) {
+			t.Parallel()
+		})
+		t.Run("should return an error when the commit type is unknown", func(t *testing.T) {
+			t.Parallel()
+		})
+		t.Run("should return an error when the commit type is empty", func(t *testing.T) {
+			t.Parallel()
+		})
+		t.Run("should return nil when the commit is valid", func(t *testing.T) {
+			t.Parallel()
+		})
+	})
+}
+
+func TestCommit_Commit(t *testing.T) {
+	t.Run("Commit.Commit", func(t *testing.T) {
+		t.Parallel()
+		t.Run("should return an error when the commit is empty", func(t *testing.T) {
+			t.Parallel()
+		})
+	})
+}
